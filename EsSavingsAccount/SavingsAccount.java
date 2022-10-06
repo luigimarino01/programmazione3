25c7d6c191a7a4d8923728c429e4ff3171c3f9fc
luigimarino01 <luigi.marino003@studenti.uniparthenope.it>
2022-10-06 15:50:14 +0200

    SavingsAccount

--- EsSavingsAccount/SavingsAccount.java
+++ EsSavingsAccount/SavingsAccount.java
@@ -3,16 +3,14 @@ import EsBank.BankAccount;
 
 public class SavingsAccount extends BankAccount {
     private float tassoDiInteresse;
-    private float tmpAmount;
     private float add;
     public SavingsAccount(float tax){
         tassoDiInteresse = tax;
     }
 
     public void addInterest(){
-        tmpAmount = this.balance();
-        add = tmpAmount*tassoDiInteresse/100;
-        this.deposit(add);
+        add = balance()*tassoDiInteresse/100;
+        deposit(add);
     }
 
 }
